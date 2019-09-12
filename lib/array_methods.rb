6ded4cf4721005d38c612b7a1656340331cef9ef
Parker Bacall <pbacall2@gmail.com>
2019-09-12 01:55:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,8 +12,8 @@ end
 def find_max_value(array)
   highest = 0
   array.length.times { |index|
-    if array[index] > array[index+1]
-        array[index] = highest
+    if array[index] < array[index+1]
+        array[index]+1 = highest
     end
   }
   return highest
