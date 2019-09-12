cb7a6d0b6c5b4325924727fd73778a4a5d734763
Parker Bacall <pbacall2@gmail.com>
2019-09-12 01:48:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,8 @@ def find_element_index(array, value_to_find)
   array.length.times { |index|
     if array[index] === value_to_find
       return index
-    end return nil
+    else return nil
+    end
   }
 end
 
